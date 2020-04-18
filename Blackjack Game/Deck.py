7c3eec4e3d6aa24cf95352facda0adbd73f91896
anaximines <miowch@gmail.com>
2020-04-18 13:13:51 +0200

    Added methods for Deck
    Shuffle deck, get the last card in the deck

--- Blackjack Game/Deck.py
+++ Blackjack Game/Deck.py
@@ -1,4 +1,5 @@
-import Card
+from Card import Card
+from random import shuffle
 
 
 class Deck:
@@ -7,11 +8,14 @@ class Deck:
     def __init__(self):
         for card_suit in ('spades', 'hearts', 'diamonds', 'clubs'):
             for card_index in range(1, 14):
-                card = Card.Card(card_suit, card_index)
+                card = Card(card_suit, card_index)
                 self.deck.append(card)
 
-    def pop(self, card_index):
-        self.deck.pop(card_index)
+    def shuffle(self):
+        shuffle(self.deck)
+
+    def give_card(self):  # removes and returns the last card in the deck
+        return self.deck.pop()
 
     def __len__(self):
         return len(self.deck)
